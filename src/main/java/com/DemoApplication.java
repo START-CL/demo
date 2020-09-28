584b1fe5b98659afed9b4ac3d9a1a0cb87b27be6
START-CL <1718577508@qq.com>
2020-09-28 23:21:37 +0800

    改动

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        /*
+         * 测试成功
+         * */
         SpringApplication.run(DemoApplication.class, args);
 
         
